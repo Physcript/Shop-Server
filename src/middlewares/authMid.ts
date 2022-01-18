29fb2a6727782cd6f21e2b7a8e672567d6e612ce
Physcript <82250688+Physcript@users.noreply.github.com>
2022-01-18 19:46:44 +0800

    update auth

--- src/middlewares/authMid.ts
+++ src/middlewares/authMid.ts
@@ -5,8 +5,8 @@ import {IUser} from "../interfaces/user";
 
 const authMid = async (req: Request,res: Response,next:NextFunction) => {
 
-    console.log(req.cookies)
-    const token = req.cookies.token
+    console.log(req.cookies)    
+    const token = req.headers.token
     console.log(token)
     await chk_token(token,res)
     next()
