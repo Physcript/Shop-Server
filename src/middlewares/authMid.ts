95e11724ea69fa9245582a7f1770f895d8fe6069
Physcript <82250688+Physcript@users.noreply.github.com>
2022-01-18 20:26:31 +0800

    Update authMid.ts

--- src/middlewares/authMid.ts
+++ src/middlewares/authMid.ts
@@ -4,13 +4,17 @@ import config from '../config'
 import {IUser} from "../interfaces/user";
 
 const authMid = async (req: Request,res: Response,next:NextFunction) => {  
-    // const token = req.headers.token || ''
-    // console.log(token)
-    // await chk_token(token,res)
-    // next()
 
-    console.log(req.headers.token)
-    console.log(typeof req.headers.token)
+    const token = req.headers.token
+    
+    jwt.verify(`${token}`,`${config.TOKEN.LOGIN}`, (err,decode) => {
+        if(err) {
+            res.locals.user = false
+        }else {
+            res.locals.user = decode
+        }
+    })
+    next()
 
 }
 export const chk_token = async (token: string, res: Response) => {
